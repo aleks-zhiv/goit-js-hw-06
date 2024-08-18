5513d24675d871a06df01fd4b355b11eb05af8a7
Oleksandr_Zhyvohliad~ <aleks-zhiv@ukr.net>
2024-08-18 16:26:21 +0300

    upd_2

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+// Задача 3. Конструктор рядків
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
